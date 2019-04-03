aba2c082330b0e318e57206d838006c86c5a3cea
Nick Leslie <nil0310@arastudent.ac.nz>
2019-04-03 18:59:41 +1300

    Iteration2: Precode, some code

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-// Iteration 1
-// 1. Write a program to play a number guessing game. The program shall generate a random number
-// between 0 and 99. The USER inputs his/her guess, and the program shall response with "Try
-// higher", "Try lower" or "You got it in n trials" if the guess is correct
+// Iteration 2
 
 // eslint-disable-next-line no-unused-vars
 class Main {
@@ -30,8 +27,24 @@ class Game {
   getGuess () {
     return this.guess
   }
-}
 
+  getRules (gameNum) {
+    const gameRules = {
+      1: `Write a program to play a number guessing game. The program shall generate a random number
+    // between 0 and 99. The USER inputs his/her guess, and the program shall response with "Try
+    // higher", "Try lower" or "You got it in n trials" if the guess is correct`,
+      2: `Write a program to play a number guessing game. The program shall generate a random number
+  between 0 and 99. The USER inputs his/her guess, and the program shall response with "COLD"
+  if the guess is more than 40 from the target number, "COOL" if the guess is within 20-39 of the
+  target number, “WARM” if the guess is within 10-19 of the target number, “HOT” if the guess is
+  within 1-9 of the target number or "You got it in n trials" if the guess is correct.`
+    }
+    return gameRules.gameNum
+  }
+}
+// 1. Write a program to play a number guessing game. The program shall generate a random number
+// between 0 and 99. The USER inputs his/her guess, and the program shall response with "Try
+// higher", "Try lower" or "You got it in n trials" if the guess is correct
 class GameOne extends Game {
   calcGuess (event) {
     this.guess = parseInt(event.target.value)
@@ -47,6 +60,21 @@ class GameOne extends Game {
   }
 }
 
+// 2. Write a program to play a number guessing game. The program shall generate a random number
+// between 0 and 99. The USER inputs his/her guess, and the program shall response with "COLD"
+// if the guess is more than 40 from the target number, "COOL" if the guess is within 20-39 of the
+// target number, “WARM” if the guess is within 10-19 of the target number, “HOT” if the guess is
+// within 1-9 of the target number or "You got it in n trials" if the guess is correct.
+class GameTwo extends Game {
+  calcGuess (event) {
+    this.guess = parseInt(event.target.value)
+    if (this.guess) {
+      this.count++
+      // TODO: New Logic
+    }
+  }
+}
+
 // CONTROLLER
 class Controller {
   loadQuestion1 () {
@@ -57,18 +85,28 @@ class Controller {
         title: 'Guess a number',
         guess: null,
         currentStatement: '',
-        game: {
-          games: [
-            new GameOne(this.guess)] },
-        count: 0
+        game: [new GameOne(this.guess), new GameTwo(this.guess)],
+        count: 0,
+        gameNum: 1
       },
       methods: {
         // Using ES6+ syntax because browser support doesn't matter
         getCalcGuess (event) {
-          console.log(this.game.games[0].getRandomNum())
-          this.currentStatement = this.game.games[0].calcGuess(event)
-          this.count = this.game.games[0].getCount()
-          this.guess = this.game.games[0].getGuess()
+          console.log(this.game[this.gameNum - 1].getRandomNum())
+          this.currentStatement = this.game[this.gameNum - 1].calcGuess(event)
+          this.count = this.game[this.gameNum - 1].getCount()
+          this.guess = this.game[this.gameNum - 1].getGuess()
+        },
+        pickGameNumber (event) {
+          this.resetElements()
+          let elementName = event.path[0].hash
+          let elementNumber = elementName[elementName.length - 1]
+          this.gameNum = elementNumber
+        },
+        resetElements () {
+          this.guess = null
+          this.currentStatement = ''
+          this.count = 0
         }
       }
     })
