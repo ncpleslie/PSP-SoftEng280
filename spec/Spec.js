0b95c7979ed189aa63dc93e1a0deb302b271fb0f
Nick Leslie <nil0310@arastudent.ac.nz>
2019-04-11 20:29:41 +1200

    Iteration2: Code, pro-docs

---
+++ spec/Spec.js
@@ -0,0 +1,158 @@
+describe('Game Setup', function () {
+  var game = new Game('This is a guess')
+
+  // Check if it returns a number
+  it('should return a random number', function () {
+    expect(game.getRandomNum()).toMatch(/\d{1,}/)
+  })
+
+  // Should return 0
+  it('Should return initial 0', function () {
+    expect(game.getCount()).toBe(0)
+  })
+
+  // Should return 'This is a guess'
+  it("Should return 'This is a guess'", function () {
+    expect(game.getGuess()).toBe('This is a guess')
+  })
+})
+
+describe('GameOne Exact Guess', function () {
+  const game = new GameOne('This is a guess')
+  game.randomNum = 50
+
+  // Check if it returns a number
+  it('should return a random number', function () {
+    expect(game.getRandomNum()).toEqual(jasmine.any(Number))
+  })
+
+  // Should return 0
+  it('Should return initial 0', function () {
+    expect(game.getCount()).toBe(0)
+  })
+
+  // Should return 'This is a guess'
+  it("Should return 'This is a guess'", function () {
+    expect(game.getGuess()).toBe('This is a guess')
+  })
+
+  const event = {
+    target: {
+      value: 50
+    }
+  }
+
+  // Should return 'You got it in 1 trials'
+  it("Should return 'You got it in 1 trials'", function () {
+    expect(game.calcGuess(event)).toBe('You got it in 1 trials')
+  })
+})
+
+describe('GameOne Low Guess', function () {
+  const game = new GameOne('This is a guess')
+  game.randomNum = 70
+  const event = {
+    target: {
+      value: 50
+    }
+  }
+
+  // Should return 'Try Higher'
+  it("Should return 'Try Higher", function () {
+    expect(game.calcGuess(event)).toBe('Try Higher')
+  })
+})
+
+describe('GameOne High Guess', function () {
+  const game = new GameOne('This is a guess')
+  game.randomNum = 30
+  const event = {
+    target: {
+      value: 50
+    }
+  }
+
+  // Should return 'Try Lower'
+  it("Should return 'Try Lower'", function () {
+    expect(game.calcGuess(event)).toBe('Try Lower')
+  })
+})
+
+// TASK TWO
+
+describe('GameTwo', function () {
+  const game = new GameTwo()
+  it('Should return random number', function () {
+    expect(game.getRandomNum()).toEqual(jasmine.any(Number))
+  })
+})
+
+describe('GameTwo', function () {
+  const game = new GameTwo()
+  game.randomNum = 99
+  let event = {
+    target: {
+      value: 1
+    }
+  }
+
+  it('Should return "COLD"', function () {
+    expect(game.calcGuess(event)).toBe('COLD')
+  })
+})
+
+describe('GameTwo', function () {
+  const game = new GameTwo()
+  game.randomNum = 35
+  event = {
+    target: {
+      value: 1
+    }
+  }
+
+  it('Should return "COOL"', function () {
+    expect(game.calcGuess(event)).toBe('COOL')
+  })
+})
+
+describe('GameTwo', function () {
+  const game = new GameTwo()
+  game.randomNum = 19
+  event = {
+    target: {
+      value: 1
+    }
+  }
+
+  it('Should return "WARM"', function () {
+    expect(game.calcGuess(event)).toBe('WARM')
+  })
+})
+
+describe('GameTwo', function () {
+  const game = new GameTwo()
+  game.randomNum = 9
+  event = {
+    target: {
+      value: 1
+    }
+  }
+
+  it('Should return "HOT"', function () {
+    expect(game.calcGuess(event)).toBe('HOT')
+  })
+})
+
+describe('GameTwo', function () {
+  const game = new GameTwo()
+  game.randomNum = 1
+  event = {
+    target: {
+      value: 1
+    }
+  }
+
+  it('Should return "You got it in 1 trials"', function () {
+    expect(game.calcGuess(event)).toBe('You got it in 1 trials')
+  })
+})
